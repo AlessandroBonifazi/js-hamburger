30a066cb6a2ff0dc38113fbc4aa79e4e2c8d3347
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-07 16:26:48 +0100

    js init

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 // Debug 
 console.log('JS OK!')
+
+// JS Hamburger
+// const hamburger = document.querySelector(".close");
+
+document.querySelector(".close").addEventListener('click',
+    function () {
+        console.log('HBG Clicked!');
+        const hamburgerNav = document.querySelector('div.hamburger-menu');
+        hamburgerNav.style.display = 'block'
+    }
+);    
